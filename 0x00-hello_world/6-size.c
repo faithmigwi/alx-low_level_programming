29eeefe319ce8772c2c9752c3f074bfd2fcf1db0
faithmigwi <faithmigwi35@gmail.com>
2023-02-16 13:34:29 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints the size of various computer types 
+ *Return 0 (Success)
+ */
+int main(void)
+(
+        char a;
+        int b;
+        long int c;
+        long long int d;
+        float f;
+printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n', (unsigned
