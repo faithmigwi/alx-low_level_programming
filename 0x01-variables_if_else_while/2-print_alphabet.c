0ecd0f5193206cecc87449a0756abf7e1565b4b6
faithmigwi <faithmigwi35@gmail.com>
2023-02-17 16:12:41 -0800

    order

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
-/**
- * main - Prints tha alphabetic
- * Return: Always (Success)
- */
-int main(void)
-{
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	putchar('\n');
-	return (0);
-}
+
